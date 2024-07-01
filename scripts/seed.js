ec83a73924732c773a5979cc333bc7c76fd8342a
Amirreza Noori <amirreza.noori@yahoo.com>
2024-07-01 22:35:03 +0330

    pg package removed in seed.js and postgres used instead.

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,36 +1,19 @@
-const pg = require('pg');
-const fs = require('fs');
-const { resolve } = require('path');
+const postgres = require('postgres');
 const { env } = require('process');
 
-const envFiles = ['.env', '.env.production', '.env.local'];
-
 require('dotenv').config({
-	path: envFiles.map((file) => resolve(process.cwd(), file)),
+	path: ['.env', '.env.production', '.env.local'],
 	override: true,
 });
 
-const client = new pg.Client({
+const sql = postgres({
 	host: env.POSTGRES_HOST,
 	database: env.POSTGRES_DATABASE,
 	user: env.POSTGRES_USER,
 	password: env.POSTGRES_PASSWORD,
 });
 
-async function main() {
-	client.connect();
-
-	const sql = await fs.readFileSync(
-		resolve(process.cwd(), 'scripts/seed.sql'),
-		{ encoding: 'utf-8' }
-	);
-
-	await client.query(sql);
-
-	await client.end();
-}
-
-main().catch((err) => {
+sql.file('./scripts/seed.sql').catch((err) => {
 	console.error(
 		'An error occurred while attempting to seed the database:',
 		err
